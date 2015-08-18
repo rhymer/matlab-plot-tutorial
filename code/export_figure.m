f1920c25ac77f91c9670899753d18c5b7381d27d
Cheng-An <rhymer123@gmail.com>
2015-08-18 16:50:25 -0700

    Prepared all the figures. Use example_code_name.m to generate all the figures. Need some fine tune and merge.

--- code/export_figure.m
+++ code/export_figure.m
@@ -1,23 +1,30 @@
-function export_figure( script_name )
+function export_figure(script_name)
 %EXPORT_FIGURE Execute the script and export the figures using the script
 %name.
-font_size = 30;
+% font_size = 30;
+% line_width = 2;
 if ischar(script_name) && exist(script_name,'file')
     close all;
     run(script_name);
-    n_figure = length(findobj('Type','figure'));
+    n_figure = length(findobj('Type', 'figure'));
     for I = 1:n_figure
         h = gcf;
         if n_figure > 1
-            file_name = sprintf('%s_%d',script_name,I);
+            file_name = sprintf('%s_%d',  script_name,I);
         else
             file_name = sprintf('%s',script_name);
         end
-        set(findall(h,'Type','text'),'FontSize',font_size);
-        set(gca,'FontSize',font_size)
-        print(h,'-depsc2',strcat('../eps/',file_name));
-        saveas(h,file_name,'fig');
+        file_name = strcat('../fig/', file_name);
+%         set(findall(h,   'Type','text'),'FontSize', font_size);
+%         set(gca, 'FontSize',font_size)
+%         set(findall(h, 'Type','line'), {'LineWidth'}, {line_width});
+        saveas(h, file_name, 'fig');
+        if exist('export_fig', 'file')
+            set(gca, 'Color', 'none');
+            export_fig(file_name, '-pdf', '-transparent')
+        else
+            print(h, '-dpdf', file_name);
+        end
     end
 end
 end
-
